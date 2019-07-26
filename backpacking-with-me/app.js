06e281062ad1fe369cc6c9878e56757376d6be95
Gloria Visus <gloriavisus@gmail.com>
2019-07-26 17:05:04 +0200

    finish auth

--- backpacking-with-me/app.js
+++ backpacking-with-me/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
+const flash = require('connect-flas');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -33,6 +34,7 @@ app.use(session({
     maxAge: 24 * 60 * 60 * 1000
   }
 }));
+app.use(flash());
 
 app.use((req, res, next) => {
   app.locals.currentUser = req.session.currentUser;
--- backpacking-with-me/routes/auth.js
+++ backpacking-with-me/routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 
 const User = require('../models/User.js');
+const { isLoggedIn, isNotLoggedIn, isFormFilled } = require('../middlewares/authMiddlewares.js');
 
 const saltRounds = 10;
 const router = express.Router();
