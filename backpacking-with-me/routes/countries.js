80fc7e4fd858ffb5bd262ba84dbdf140888903e5
Anna Trogimova <cobrana1000@gmail.com>
2019-07-27 16:51:20 +0200

    country details

--- backpacking-with-me/routes/countries.js
+++ backpacking-with-me/routes/countries.js
@@ -5,14 +5,27 @@ const Country = require('../models/Country.js');
 
 
 
-/* GET users listing. */
+ /* GET users listing. */
 router.get('/', async (req, res, next) => {
   try {
     const countryList = await Country.find();
-    res.render('index', { countryList });
+    res.render('countryView/indexCountry', { countryList });
   } catch (error) {
     next(error);
   }
 });
 
-module.exports = router;
+
+router.get('/:id', async (req, res, next) =>{
+  try{
+    const { id } = req.params;
+    console.log(id)
+    const countryDetail = await Country.findById(id);
+    console.log(countryDetail);
+    res.render('countryView/countryDetail', {countryDetail});
+  }
+  catch(eror){
+    next(error);
+  }
+})
+ module.exports = router;
