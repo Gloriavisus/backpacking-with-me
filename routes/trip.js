6aaeb4447619efc25447b65655b4c8ca15f0d719
Anna Trogimova <cobrana1000@gmail.com>
2019-07-31 15:03:08 +0200

    trip search

--- routes/trip.js
+++ routes/trip.js
@@ -9,22 +9,27 @@ router.get('/', async (req, res, next) => {
 
 });
 
-router.post('/', async (req, res, next) => {
-  const { countryFrom, countryTo, dateFrom, dateTo, price, fly_duration } = req.body;
+router.post('/:id/getFlights', async (req, res, next) => {
+  const { countryFrom, countryTo, dateFrom, dateTo } = req.body;
+  const { id } = req.params;
 
   try {
     // console.log('getting ingo', dateFrom, dateTo);
     const getFlightInfo = await axios.get(`https://api.skypicker.com/flights?flyFrom=${countryFrom}&to=${countryTo}&dateFrom=${dateFrom}&dateTo=${dateTo}&partner=picky&one_for_city=1`);
-    console.log(getFlightInfo.data.data);
+
     // return flight information from search
 
     // 1) console log what get getFlightInfo.data.data[0];
     // 2) choose the fields you want from this and put them inside the const {cityFrom}
 
-    const { cityFrom } = getFlightInfo.data.data[0];
+    const { cityFrom, cityTo, price, fly_duration } = getFlightInfo.data.data[0];
     // 3) put these fields inside the render method below cityFrom
     return res.render('trip', {
-      cityFrom
+      cityFrom,
+      cityTo,
+      price,
+      fly_duration,
+      id
     });
   } catch (error) {
     next(error);
@@ -41,16 +46,26 @@ router.get('/:id', async (req, res, next) => {
           if (trip._id.toString() === id) { tripUsers.push(user); };
         });
       });
-      console.log(tripUsers);
+
+      console.log(id);
       // render trip landing page
-      res.render('trip', { tripUsers });
+
+      res.render('trip', { tripUsers, id: id.toString() });
     });
 });
 
 // a post to update the trip with the information from the api get above
+router.post('/:tripId/save', async (req, res, next) => {
+  const { tripId } = req.params;
+  const { cityFrom, cityTo, price, flyDuration } = req.body;
 
+  Trip.findByIdAndUpdate(tripId, { countryFrom: cityFrom, countryTo: cityTo, price, fly_duration: flyDuration }, { new: true })
+    .then((data) => { console.log(data); })
+    .catch((error) => { console.log(error); });
+});
 // a post to create a trip from city
 router.post('/:id/:country', async (req, res, next) => {
+  // maybe id is the id of the other user and not the trip
   const { id, country } = req.params;
   const myUserId = req.session.currentUser._id;
 
@@ -58,10 +73,10 @@ router.post('/:id/:country', async (req, res, next) => {
     countryTo: country
   });
   const saveTrip = newTrip.save();
-  const updateOtherUser = User.findByIdAndUpdate(id, { $push: { trips: newTrip._id } });
-  const updateMyUser = User.findByIdAndUpdate(myUserId, { $push: { trips: newTrip._id } });
+  const updateOtherUser = User.findByIdAndUpdate(id, { $push: { trips: newTrip._id } }, { new: true });
+  const updateMyUser = User.findByIdAndUpdate(myUserId, { $push: { trips: newTrip._id } }, { new: true });
   Promise.all([updateMyUser, updateOtherUser, saveTrip])
-    .then((data) => res.redirect(`/trip/${id}`))
+    .then((data) => res.redirect(`/trip/${newTrip._id}`))
     .catch((error) => { next(error); });
 });
 module.exports = router;
